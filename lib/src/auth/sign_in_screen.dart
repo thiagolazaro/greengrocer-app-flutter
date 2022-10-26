07d5aae651fe88205c1572125e04be1f9220fb9d
Thiago Lázaro <thiagolazaro2009@gmail.com>
2022-10-26 14:13:05 -0300

    Criando o Botão de Cadastro e Estruturando sem Expanded

--- lib/src/auth/sign_in_screen.dart
+++ lib/src/auth/sign_in_screen.dart
@@ -15,69 +15,111 @@ class SignInScreen extends StatelessWidget {
               color: Colors.red,
             ),
           ),
-          Expanded(
-            child: Container(
-              padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 40),
-              decoration: const BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.vertical(
-                  top: Radius.circular(45),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 40),
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.vertical(
+                top: Radius.circular(45),
+              ),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                // E-mail
+                const CustomTextField(
+                  icon: Icons.email,
+                  label: 'E-mail',
                 ),
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  // E-mail
-                  const CustomTextField(
-                    icon: Icons.email,
-                    label: 'E-mail',
-                  ),
 
-                  // Senha
-                  const CustomTextField(
-                    icon: Icons.lock,
-                    label: 'Senha',
-                    isSecret: true,
-                  ),
+                // Senha
+                const CustomTextField(
+                  icon: Icons.lock,
+                  label: 'Senha',
+                  isSecret: true,
+                ),
 
-                  // Botão Entrar
-                  SizedBox(
-                    height: 50,
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(18),
-                        ),
+                // Botão Entrar
+                SizedBox(
+                  height: 50,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18),
                       ),
-                      onPressed: () {},
-                      child: const Text(
-                        'Entar',
-                        style: TextStyle(
-                          fontSize: 18,
-                        ),
+                    ),
+                    onPressed: () {},
+                    child: const Text(
+                      'Entar',
+                      style: TextStyle(
+                        fontSize: 18,
                       ),
                     ),
                   ),
+                ),
 
-                  // Botão Esqueceu Senha
-                  Align(
-                    alignment: Alignment.centerRight,
-                    child: TextButton(
-                      onPressed: () {},
-                      child: const Text(
-                        'Esquceu a senha?',
-                        style: TextStyle(
-                          color: Colors.red,
-                        ),
+                // Botão Esqueceu Senha
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      'Esquceu a senha?',
+                      style: TextStyle(
+                        color: Colors.red,
                       ),
                     ),
-                  )
+                  ),
+                ),
 
-                  // Separador
+                // Divisor
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 10),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Divider(
+                          color: Colors.grey.withAlpha(90),
+                          thickness: 1,
+                        ),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 15),
+                        child: Text('Ou'),
+                      ),
+                      Expanded(
+                        child: Divider(
+                          color: Colors.grey.withAlpha(90),
+                          thickness: 1,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
 
-                  // Criar Conta
-                ],
-              ),
+                // Criar Conta
+                SizedBox(
+                  height: 50,
+                  child: OutlinedButton(
+                    style: OutlinedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18),
+                      ),
+                      side: const BorderSide(
+                        width: 2,
+                        color: Colors.green,
+                      ),
+                    ),
+                    onPressed: () {},
+                    child: const Text(
+                      'Criar conta',
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
         ],
