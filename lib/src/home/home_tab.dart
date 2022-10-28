dece49360f45226bfda795171c378ad47ba356b2
Thiago Lázaro <thiagolazaro2009@gmail.com>
2022-10-28 14:22:01 -0300

    Badge e Campo de Pesquisa

--- lib/src/home/home_tab.dart
+++ lib/src/home/home_tab.dart
@@ -1,3 +1,4 @@
+import 'package:badges/badges.dart';
 import 'package:flutter/material.dart';
 import 'package:greengrocer/src/config/custom_colors.dart';
 
@@ -12,6 +13,7 @@ class HomeTab extends StatelessWidget {
         elevation: 0,
         backgroundColor: Colors.transparent,
         centerTitle: true,
+        // Nome do App
         title: Text.rich(
           TextSpan(
             style: const TextStyle(fontSize: 30),
@@ -34,17 +36,63 @@ class HomeTab extends StatelessWidget {
 
         // Icone Carrinho
         actions: [
-          IconButton(
-            onPressed: () {},
-            icon: Icon(
-              Icons.shopping_cart,
-              color: CustomColors.customSwatchColor,
+          Padding(
+            padding: const EdgeInsets.only(top: 15, right: 15),
+            child: GestureDetector(
+              onTap: () {},
+              child: Badge(
+                badgeColor: CustomColors.customContrastColor,
+                badgeContent: const Text(
+                  '2',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 12,
+                  ),
+                ),
+                child: Icon(
+                  Icons.shopping_cart,
+                  color: CustomColors.customSwatchColor,
+                ),
+              ),
             ),
           ),
         ],
       ),
 
-      // Campo de pesquisa
+      body: Column(
+        children: [
+          // Campo de pesquisa
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: 10,
+            ),
+            child: TextFormField(
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
+                isDense: true,
+                hintText: 'Pesquise aqui...',
+                prefixIcon: Icon(
+                  Icons.search,
+                  color: CustomColors.customContrastColor,
+                  size: 20,
+                ),
+                hintStyle: TextStyle(
+                  color: Colors.grey.shade400,
+                  fontSize: 14,
+                ),
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(60),
+                    borderSide: const BorderSide(
+                      width: 0,
+                      style: BorderStyle.none,
+                    )),
+              ),
+            ),
+          )
+        ],
+      ),
 
       // Categorias
 
