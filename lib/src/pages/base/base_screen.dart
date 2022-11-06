c874932f25e2eb55d4c5717dda72dc254db1fd44
Thiago Lázaro <thiagolazaro2009@gmail.com>
2022-11-05 21:08:28 -0300

    Animação na Navegação do PageView

--- lib/src/pages/base/base_screen.dart
+++ lib/src/pages/base/base_screen.dart
@@ -36,7 +36,12 @@ class _BaseScreenState extends State<BaseScreen> {
         onTap: (index) {
           setState(() {
             currentIndex = index;
-            pageController.jumpToPage(index);
+            // pageController.jumpToPage(index);
+            pageController.animateToPage(
+              index,
+              duration: const Duration(milliseconds: 500),
+              curve: Curves.ease,
+            );
           });
         },
         type: BottomNavigationBarType.fixed,
